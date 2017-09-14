b716943a18adc5d38184411fa502e4a6e9ddd249
wan-ls <wan-ls@foxmail.com>
2017-09-14 10:54:00 +0800

    可以看看页面

--- src/cn/MS/controller/WebController.java
+++ src/cn/MS/controller/WebController.java
@@ -1,10 +1,23 @@
 package cn.MS.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
 public class WebController {
-   @RequestMapping("/")
+   @RequestMapping("/index")
    public String goIndex(){
 	   return "index";
+   }
+   @RequestMapping("/home")
+   public String goHome() {
+	   return "home";
+   }
+   @RequestMapping("/login")
+   public String login() {
+	   return "login";
+   }
+   @RequestMapping("/admin")
+   public String admin() {
+	   return "AdminManage";
    }
 }
